2dc55673d9465132332698ca191d00f490b8f27e
Someone Account <158297903+Yenot52@users.noreply.github.com>
2025-01-11 21:36:29 +0200

    updates

--- library/author/models.py
+++ library/author/models.py
@@ -1,32 +1,19 @@
 from django.db import models
 
-
-
 class Author(models.Model):
     """
-        This class represents an Author. \n
-        Attributes:
-        -----------
-        param name: Describes name of the author
-        type name: str max_length=20
-        param surname: Describes last name of the author
-        type surname: str max_length=20
-        param patronymic: Describes middle name of the author
-        type patronymic: str max_length=20
+    This class represents an Author.
     """
     name = models.CharField(blank=True, max_length=20)
     surname = models.CharField(blank=True, max_length=20)
     patronymic = models.CharField(blank=True, max_length=20)
-    books = models.ManyToManyField('book.Book', related_name='authors')
     id = models.AutoField(primary_key=True)
+    
+    books = models.ManyToManyField('book.Book', related_name='authors_of_book')
 
     def __str__(self):
-        """
-        Magic method is redefined to show all information about Author.
-        :return: author id, author name, author surname, author patronymic
-        """
-        return f"\'id\': {self.pk}, \'name\': \'{self.name}\'," \
-               f" \'surname\': \'{self.surname}\', \'patronymic\': \'{self.patronymic}\'"
+        return f"\'id\': {self.pk}, \'name\': \'{self.name}\', \'surname\': \'{self.surname}\', \'patronymic\': \'{self.patronymic}\'"
+
 
     def __repr__(self):
         """
--- library/book/models.py
+++ library/book/models.py
@@ -1,34 +1,15 @@
 from django.db import models
 from author.models import Author
 
-
-
 class Book(models.Model):
-    """
-        This class represents an Author. \n
-        Attributes:
-        -----------
-        param name: Describes name of the book
-        type name: str max_length=128
-        param description: Describes description of the book
-        type description: str
-        param count: Describes count of the book
-        type count: int default=10
-        param authors: list of Authors
-        type authors: list->Author
-    """
+
     name = models.CharField(blank=True, max_length=128)
     description = models.CharField(blank=True, max_length=256)
     count = models.IntegerField(default=10)
     id = models.AutoField(primary_key=True)
-    authors = models.ManyToManyField('author.Author', related_name='books')
-    # authors = models.ManyToManyField(Author, related_name='books')
+    authors = models.ManyToManyField('author.Author', related_name='written_books')
 
     def __str__(self):
-        """
-        Magic method is redefined to show all information about Book.
-        :return: book id, book name, book description, book count, book authors
-        """
         return f"'id': {self.id}, 'name': '{self.name}', 'description': '{self.description}', 'count': {self.count}, 'authors': {[author.id for author in self.authors.all()]}"
 
     def __repr__(self):
