1520974179f34b4259a3e18389d941586ab90aee
Someone Account <158297903+Yenot52@users.noreply.github.com>
2025-01-11 21:52:12 +0200

    last version

--- library/book/views.py
+++ library/book/views.py
@@ -3,7 +3,7 @@ from .models import Book
 from order.models import Order
 
 def book_list(request):
-    books = Book.objects.all()
+    book = Book.objects.all()
     return render(request, 'book/book_list.html', {'book': book})
 
 def book_detail(request, book_id):
@@ -12,10 +12,10 @@ def book_detail(request, book_id):
 
 def book_filter(request):
     query = request.GET.get('q', '')
-    books = Book.objects.filter(name__icontains=query)
+    book = Book.objects.filter(name__icontains=query)
     return render(request, 'book/book_filter.html', {'book': book, 'query': query})
 
 def book_by_user(request, user_id):
-    orders = Order.objects.filter(user_id=user_id).select_related('book')
-    books = [order.book for order in orders]
+    order = Order.objects.filter(user_id=user_id).select_related('book')
+    book = [order.book for order in order]
     return render(request, 'book/book_by_user.html', {'book': book})
